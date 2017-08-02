35a93dab2e380fcaa4c3d86059af7cb470db21d6
Douglass Hooker <DouglassHooker@gmail.com>
2017-08-01 22:11:27 -0500

    Changed to a better color scheme, blue main with charcoal accents.
    Very Nice

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,42 +1,27 @@
 //*********************************************************//
-//             I found this on CSS-Tricks a                //
-//https://css-tricks.com/snippets/jquery/smooth-scrolling/ //
-//                                                         //
+//             I found this on Stack Overflow              //
+//https://jsfiddle.net/o92wrpg8/2/                         //
+//https://stackoverflow.com/questions/18891188/twitter-\   //
+//bootstrap-scrollspy-disable-on-smooth-scroll#18895225    //
 //*********************************************************//
 
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 500, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
+$("body").scrollspy({
+  target: ".active-spy"
+});
+
+$(document).on('click', 'a', function(event) {
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, {
+    duration: 750,
+    start: function() {
+      $("#nav").removeClass("active-spy");
+    },
+    complete: function() {
+      $("#nav").addClass("active-spy");
+      $(".active").removeClass("active");
+      $(event.target.parentNode).addClass("active");
     }
   });
+});
+
